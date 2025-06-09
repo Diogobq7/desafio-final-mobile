060f85c5fe0ad6b39913672ecaf40320b7119cf9
Diogobq7 <diogo.queiroz@germinare.org.br>
2025-06-09 20:02:39 -0300

    Subindo alterações no DB

--- app/src/main/java/com/aula/desafio_mobile/AdapterAtendimento.kt
+++ app/src/main/java/com/aula/desafio_mobile/AdapterAtendimento.kt
@@ -4,22 +4,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterAtendimento(private val notas: MutableList<Atendimento> = mutableListOf()) : RecyclerView.Adapter<AdapterAtendimento.ViewHolder>() {
+class AdapterAtendimento(private val atendimentos: MutableList<Atendimento> = mutableListOf()) : RecyclerView.Adapter<AdapterAtendimento.ViewHolder>() {
     private val db = Database()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterAtendimento.ViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout., parent, false)
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_atendimento, parent, false)
         return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return atendimentos.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.nome.text = atendimentos[position].getNome()
+        holder.entrada.text = atendimentos[position].getEntrada()
+        holder.saida.text = atendimentos[position].getSaida()
+
+        holder.itemView.setOnLongClickListener {
+            Toast.makeText(it.context, "Aqui abrirá um popup de confirmação", Toast.LENGTH_SHORT).show()
+            true
+        }
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/com/aula/desafio_mobile/Database.kt
+++ app/src/main/java/com/aula/desafio_mobile/Database.kt
@@ -1,6 +1,7 @@
 package com.aula.desafio_mobile
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -25,12 +26,37 @@ class Database {
 
         db.collection("atendimento").document(argAtendimento.getId().toString())
             .delete().addOnSuccessListener {
-                Toast.makeText(c, "Nota removida com sucesso!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(c, "Atendimento removido com sucesso!", Toast.LENGTH_SHORT).show()
             }
     }
 
     fun listar(argAtendimento: MutableList<Atendimento>, argAdapter: AdapterAtendimento, c: Context){
+        val db = abrirDB()
 
+        db.collection("ListaNotas")
+            .addSnapshotListener { snapshot, error ->
+                if (error != null) {
+                    Log.w("Firestore", "Erro ao ouvir mudanças", error)
+                    Toast.makeText(c, "Você está off-line neste momento!", Toast.LENGTH_SHORT).show()
+                    return@addSnapshotListener
+                }
+
+                if (snapshot != null) {
+                    argAtendimento.clear() // Limpa a lista antes de adicionar os novos itens
+
+                    for (document in snapshot.documents) {
+                        val atendimento = document.toObject(Atendimento::class.java)
+                        atendimento?.let {
+                            argAtendimento.add(it)
+                        }
+                    }
+
+                    argAdapter.notifyDataSetChanged()
+                } else {
+                    Log.d("Firestore", "Nenhum dado encontrado")
+                    Toast.makeText(c, "Nenhum dado encontrado", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
 
