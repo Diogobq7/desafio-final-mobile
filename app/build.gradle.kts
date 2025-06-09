1221d87f95cd66cff3ca28e7606000484821e65f
RaphaelaMariaGomes <raphaelamsg@gmail.com>
2025-06-09 15:14:44 -0300

    Atualizando as dependencias no gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
