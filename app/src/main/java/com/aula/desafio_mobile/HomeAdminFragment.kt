2beb66bda14ec7e533652b1f07636dfcd47d6747
RaphaelaMariaGomes <raphaelamsg@gmail.com>
2025-06-12 07:23:36 -0300

    Fazendo o homeAdminFragment

--- app/src/main/java/com/aula/desafio_mobile/HomeAdminFragment.kt
+++ app/src/main/java/com/aula/desafio_mobile/HomeAdminFragment.kt
@@ -1,59 +1,60 @@
 package com.aula.desafio_mobile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aula.desafio_mobile.databinding.NavigationHomeAdminBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeAdminFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeAdminFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: NavigationHomeAdminBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var adapterAtendimento: AdapterAtendimento
+    private val atendimentos = mutableListOf<Atendimento>()
+    private val db = Database()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = NavigationHomeAdminBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+        setupFirestoreListener()
+    }
+
+    private fun setupRecyclerView() {
+        adapterAtendimento = AdapterAtendimento(atendimentos)
+
+        binding.rvAdmin.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = adapterAtendimento
+            addItemDecoration(
+                DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+            )
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.navigation_home_admin, container, false)
+    private fun setupFirestoreListener() {
+        db.listar(atendimentos, adapterAtendimento, requireContext())
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeAdminFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeAdminFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun addAtendimento(atendimento: Atendimento) {
+        db.salvar(atendimento, requireContext())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
